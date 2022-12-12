ae6eb0eb3849503c0e702ce4fdf273f5de943c76
MarinaSmetanina1977Mosk <msmetanina@mail.ru>
2022-12-12 18:59:06 +0300

    message

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿Random random = new Random();
+double[]arr=new double [7];
+for(int i=0; i<arr.Length;i++)
+    arr[i]=random.Next(0,100);
+Console.WriteLine(string.Join(",",arr));
+Console.WriteLine("Разница между максимальным и минимальным элементами:"+DiffMaxMin(arr));
+double DiffMaxMin(double[] array)
+{
+    int i=1;
+    double diff=0,max=array[0],min=array[0];
+    for (i=1;i<array.Length;i++)
+    {
+        if (array[i]>max)
+        max=array[i];
+        if (array[i]<min)
+        min=array[i];
+    }
+    diff=max-min;
+    return diff;
+
+}
+
